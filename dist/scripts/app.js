5b2703e1d817eb414341b6c2d1ff4095aab62b0e
proll <gleb.proll@gmail.com>
2014-10-19 18:08:03 +0400

    переверстана по требованиям collection и придумал как хитро позиционировать крестики

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,6 +1,7 @@
 $(function() {
 	var $overlay = $('.overlay'),
-		$body = $('body');
+		$body = $('body'),
+		$window = $(window);
 	$overlay.find('.overlay__close').on('touchstart click', function(e) {
 			if(e && e.preventDefault) {
 				e.preventDefault();
@@ -41,4 +42,74 @@ $(function() {
 		$edit.attr('placeholder', placeholder_text);
 		return true;
 	})
+
+	var $collections,
+		$collection,
+		$marks,
+		$mark,
+		iw = 768,				// w of image
+		ih = 1004,				// h of image
+		cw,						// w of collection
+		ch,						// h of collection
+		kw,						// cw/iw
+		kh,						// ch/ih
+		ki = iw/ih,				// w/h for image
+		kc,						// w/h for collection
+		k = 0,					// K for scaling coordinates
+		translate = '',			// string with css translate for mark
+		mx, 					// mark x translate
+		my,						// mark y translate
+		mmargin = 10;			// margin from end of container
+
+	function positionMark() {
+		_.forEach($collections, function(collection, i) {
+			$collection = $(collection);
+			cw = $collection.outerWidth();
+			ch = $collection.outerHeight();
+			kw = cw/iw;
+			kh = ch/ih;
+			kc = cw/ch;
+
+			if(kc>=ki) {
+				k = kw;
+			} else {
+				k = kh;
+			}
+
+			$marks = $collection.find('.collection__item-mark');
+			_.forEach($marks, function(mark, i) {
+				$mark = $(mark);
+				mx = k*($mark.data('x')-iw/2);
+				my = k*($mark.data('y')-ih/2);
+				if(mx < -cw/2 + mmargin) {
+					mx = -cw/2 + mmargin;
+				} else if(mx > cw/2 - mmargin) {
+					mx = cw/2 - mmargin;
+				}
+				if(my < -ch/2 + mmargin) {
+					my = -ch/2 + mmargin;
+				} else if(my > ch/2 - mmargin) {
+					my = ch/2 - mmargin;
+				}
+
+				translate = 'translate(' + mx + 'px, ' + my + 'px)'; 
+				$mark.css({
+					'-webkit-transform': translate,
+					   '-moz-transform': translate,
+					    '-ms-transform': translate,
+					     '-o-transform': translate,
+					        'transform': translate
+				})
+			});
+		});
+	}
+
+	// if it is "collection" site
+	if(location.href.indexOf('/collection')!==-1) {
+		$collections = $('.collection__grid-itm');
+		positionMark();
+		$window.on('resize', _.throttle(_.bind(positionMark, this), 500));
+
+	}
+
 })
